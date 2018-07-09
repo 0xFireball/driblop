f65efc3bb72126f1a622794df04e32bb73b41af8
0xFireball <0xFireball@outlook.com>
2018-07-09 10:19:51 -0500

    create project

---
+++ src/Driblop/Utilities/JsonNetResponseSerializerExtension.cs
@@ -0,0 +1,11 @@
+using Nancy;
+using Newtonsoft.Json;
+
+namespace Driblop.Utilities {
+    public static class JsonNetResponseSerializerExtension {
+        public static Response asJsonNet<T>(this IResponseFormatter formatter, T instance) {
+            var responseData = JsonConvert.SerializeObject(instance);
+            return formatter.AsText(responseData, "application/json");
+        }
+    }
+}
